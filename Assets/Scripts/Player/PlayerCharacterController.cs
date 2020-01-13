688c2a0632209bd3a28b0259947609c6aff3e52f
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-01-12 18:58:51 -0800

    Cleaned code, Fixed input
    Attempted vaulting but its not working yet

--- Assets/Scripts/Player/PlayerCharacterController.cs
+++ Assets/Scripts/Player/PlayerCharacterController.cs
@@ -15,10 +15,13 @@ public class PlayerCharacterController : MonoBehaviour
     public float SlideSpeed = 3000f;
     public float slidedec = 10f;
 
-    //public Transform GroundCheck;
+    public Transform GroundCheck;
     //Radius for checking for floor
-    //public float GroundDistance = 0.4f;
-    //public LayerMask GorundMask;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+    public LayerMask VualtLayer;
+    public LayerMask WallLayer;
+
 
     public float jetpackfuel; //current fuel in the jetpack
 
@@ -34,9 +37,9 @@ public class PlayerCharacterController : MonoBehaviour
 
     public bool IsCrouching; //keeps track if the palyer is crouching or not
     private float height;
+    private float radius;
     public bool CanStand;//wont let palyer stands if something is blocking him
     private float CrouchTimer;//sets a timer before the palyer can double jump
-
 
     Vector3 SlideForce; //Force for sliding;
     public bool sliding;
@@ -44,38 +47,52 @@ public class PlayerCharacterController : MonoBehaviour
     private float TempSlideSpeed;
     private Vector3 slideDIR;
 
-
+    public bool IsTouchingWall;
+    public bool IsRoomForClimb;
 
     // Start is called before the first frame update
     void Start()
     {
         jetpackfuel = 10f;
         height = CharController.height;
+        radius = CharController.radius;
         IsCrouching = false;
+
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    //fixed update and update contain separate functions for each movement
+    //functions are odered in what step they should be executed
+    void Update()//things that need to happen only once per frame
+    {
+        Jump();
+        Vualt();
+        Climb();
+    }
+
+    void FixedUpdate()//things that need to happen multiple times per frame
     {
         //Check in a sphere if the floor is in range, like a collider check
-        //IsGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask, QueryTriggerInteraction.Ignore);
-        IsGrounded = CharController.isGrounded;
+        IsGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask, QueryTriggerInteraction.Ignore);
+        if (!IsGrounded)
+        {
+            IsGrounded = CharController.isGrounded;
+        }
+                      
 
-        //handles Moving ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
 
-        //transform.right and transform.forward uses local coords instead of world coords
-        Vector3 move = transform.right * x + transform.forward * z;
+        JetPack();//accepst continuous input for jetpack
+        Crouch();//accepts continuous input for sliding and crouching
+        Slide();//exectues sliding force
+        Movement();//executes movement force
 
-        CharController.Move(move * MoveSpeed * Time.deltaTime);
+    }
 
-        //handles jumping ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    void Jump()
+    {
         //reset double jump if the ground is touched
         if (IsGrounded && DblJump == 2)
         {
-            DblJump = 1;
+            DblJump = 0;
             JumpTimer = 0;
         }
 
@@ -87,7 +104,7 @@ public class PlayerCharacterController : MonoBehaviour
             DblJump = 1;
         }
 
-        if (Input.GetAxis("Jump") > 0 && !IsGrounded && DblJump == 1 && Input.GetAxis("Sprint") == 0 && JumpTimer > .3f) // if jumping in air and not using jetpack
+        else if (Input.GetButtonDown("Jump") && !IsGrounded && DblJump == 1 && Input.GetAxis("Sprint") == 0) // if jumping in air and not using jetpack    && JumpTimer > .3f
         {
             Velocity.y = Mathf.Sqrt(JumpHeight * -2f * Gravity);
             DblJump = 2;
@@ -97,9 +114,35 @@ public class PlayerCharacterController : MonoBehaviour
         {
             JumpTimer += Time.deltaTime;
         }
+    }
+       
+    void Vualt() {
 
-        //handles jetpack ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    }
+
+    void Climb(){
+        //if(Physics.SphereCast(transform.position, 0.25f, transform.forward, out var hit, 2f, WallLayer))//wall hit
+        Vector3 pos = transform.position + (Vector3.up * height / 3f) + (transform.forward * radius / 2f);
+        Vector3 dir = transform.TransformDirection(new Vector3(0, -0.5f, 1).normalized);
+
+        if(Physics.SphereCast(pos, 0.2f, dir, out var hit, 3f, WallLayer))//wall hit
+        {
+            Debug.DrawRay(pos, dir * 5, Color.green);
+
+            pos = pos + (transform.forward * radius) + (transform.up * radius);
+            Debug.DrawRay(pos, transform.up * 5, Color.red);
+            
+            if (Physics.SphereCast(pos, 0.2f, transform.up, out var hit2, 5f, WallLayer))
+            {
+                Debug.Log(hit2.point);
+
+            }
+        }
+    }
+
+    void JetPack()
+    {
         //Check if the player is on the ground but was falling at some point
         if (IsGrounded && Velocity.y < 0)
         {
@@ -130,19 +173,10 @@ public class PlayerCharacterController : MonoBehaviour
             //Velocity.y += Gravity * Time.deltaTime;
             Velocity += Vector3.up * Gravity * (fallmult - 1) * Time.deltaTime; // increases fall gravity for better feel
         }
-
-        //applies forces on the y axis from jumping or gravity or jetpack////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //-9.81m/s * t * t
-        CharController.Move(Velocity * Time.deltaTime);
-
-        //Optional movement systems /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        Crouch();//accepts input for sliding and crouching
-        Slide();//exectues sliding
-        Vualt();
-        Climb();
     }
 
-    void Crouch() {//choppy returning to max hieght is instant and feels like teleporting
+    void Crouch()
+    {//choppy returning to max hieght is instant and feels like teleporting
         if (IsGrounded)
         {
             if (Input.GetAxis("Crouch") > 0 && Input.GetAxis("Sprint") > 0 && !IsCrouching && CrouchTimer >= .3f && sliding == false)//player will slide
@@ -162,7 +196,7 @@ public class PlayerCharacterController : MonoBehaviour
                 CrouchTimer = 0;
             }
 
-            else if (Input.GetAxis("Crouch") > 0 && IsCrouching && CanStand && CrouchTimer >= .2f)//if there is nothing over the character he can stand back up
+            else if ((Input.GetAxis("Crouch") > 0 || Input.GetAxis("Jump") > 0) && IsCrouching && CanStand && CrouchTimer >= .2f)//if there is nothing over the character he can stand back up
             {
                 IsCrouching = false;
                 sliding = false;
@@ -175,19 +209,17 @@ public class PlayerCharacterController : MonoBehaviour
                 CrouchTimer += Time.deltaTime;
             }
         }
-
         if (Physics.Raycast(transform.position, transform.up, out var hit, 3))//check to make sure there is nothing over the character
         {
             CanStand = false;
         }
-        else 
+        else
         {
             CanStand = true;
         }
-
     }
-
-    void Slide() {
+    void Slide()
+    {
         if (sliding)//want to add behavior where slide will keep going and increase if going down an incline
         {
             if (firstslide)//initial slide speed
@@ -210,7 +242,6 @@ public class PlayerCharacterController : MonoBehaviour
             {
                 SlideForce.y = 0;
             }
-            Debug.Log(SlideForce);
             CharController.Move(SlideForce * Time.deltaTime);
         }
 
@@ -220,11 +251,18 @@ public class PlayerCharacterController : MonoBehaviour
         }
     }
 
-    void Vualt() {
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-    }
+        //transform.right and transform.forward uses local coords instead of world coords
+        Vector3 move = transform.right * x + transform.forward * z;
 
-    void Climb(){
-    
+        CharController.Move(move * MoveSpeed * Time.deltaTime);
+
+        //applies forces on the y axis from jumping or gravity or jetpack
+        //-9.81m/s * t * t
+        CharController.Move(Velocity * Time.deltaTime);
     }
 }
