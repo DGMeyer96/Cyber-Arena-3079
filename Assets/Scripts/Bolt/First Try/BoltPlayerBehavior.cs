cab2cffabadec8b82525f4a32d8b0b2e04702610
Stephen <brokensteelstring@gmail.com>
2020-01-29 22:31:39 -0800

    Whole lotta networking tomfoolery I tell ya what

---
+++ Assets/Scripts/Bolt/First Try/BoltPlayerBehavior.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltPlayerBehavior : Bolt.EntityBehaviour<IPlayerState>
+{
+    //public CharacterController CharController;
+    //private Vector3 Velocity;
+
+
+    public override void Attached()
+    {
+        //CharController = gameObject.GetComponent<CharacterController>();
+        state.SetTransforms(state.PlayerTransform, transform);
+    }
+
+    public override void SimulateOwner()
+    {
+        /*
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //transform.right and transform.forward uses local coords instead of world coords
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        CharController.Move(move * 12f * Time.deltaTime);
+        */
+        //applies forces on the y axis from jumping or gravity or jetpack
+        //-9.81m/s * t * t
+        //CharController.Move(Velocity * Time.deltaTime);
+
+        var speed = 12f;
+        var movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) { movement.z += 1; }
+        if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
+        if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movement.x += 1; }
+
+        if (movement != Vector3.zero)
+        {
+            transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Bolt/First Try/Menu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using Bolt.Matchmaking;
+using System;
+using UdpKit;
+
+public class Menu : Bolt.GlobalEventListener
+{
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+
+        if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+        {
+            // START SERVER
+            BoltLauncher.StartServer();
+        }
+
+        if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+        {
+            // START CLIENT
+            BoltLauncher.StartClient();
+        }
+
+        GUILayout.EndArea();
+    }
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            string matchName = Guid.NewGuid().ToString();
+
+            BoltMatchmaking.CreateSession(
+                sessionID: matchName,
+                sceneToLoad: "Bolt_MP_2",
+                token: null
+            );
+        }
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
+
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltNetwork.Connect(photonSession);
+            }
+        }
+    }
+}
