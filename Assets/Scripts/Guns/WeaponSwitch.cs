8bcb3f6b56559a8c4bb349dbc124f461bf12c9f6
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-02-22 14:15:40 -0800

    Fixed Weapon Switch  Started WeaponTracker V1
    Weapon Switch now no longer needs a specified size
    
    Weapon Tracker  tracks current ammo for all guns.  Script is set up to only hold 1 of each weapon type.  Weapons can be switched to only when that weapon has ammo.  
    
    Idea: if a weapon has ammo it is display in INV.  if it does not have ammo it will be discarded until ammo is found.

--- Assets/Scripts/Guns/WeaponSwitch.cs
+++ Assets/Scripts/Guns/WeaponSwitch.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class WeaponSwitch : MonoBehaviour
 {
+    public AmmoTracker AmmoTracker;
+    public bool useable;
+    public int CurrentAmmo;
     public int SelectedWeapon = 0;
+    public int NumOfWeapons;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,40 +16,74 @@ public class WeaponSwitch : MonoBehaviour
     }
 
     // Update is called once per frame
+
     void Update()
     {
+        NumOfWeapons = transform.childCount;
+
         int PreviousSelectedWeapon = SelectedWeapon;
-        //if (Input.GetAxis("Mouse ScrollWheel") > 0) 
-        //{
-        //    if (SelectedWeapon >= transform.childCount - 1)
-        //    {
-        //        SelectedWeapon = 0;
-        //    }
-        //    SelectedWeapon++;
-        //}        
-        //if (Input.GetAxis("Mouse ScrollWheel") < 0) 
-        //{
-        //    if (SelectedWeapon <= transform.childCount - 1)
-        //    {
-        //        SelectedWeapon = 0;
-        //    }
-        //    SelectedWeapon--;
-        //}
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (SelectedWeapon >= NumOfWeapons - 1)
+            {
+                SelectedWeapon = 0;
+            }
+            else 
+            {
+                SelectedWeapon++;
+            }
+        }        
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) 
+        {
+            if (SelectedWeapon == 0)
+            {
+                SelectedWeapon = NumOfWeapons - 1;
+            }
+            else 
+            {
+                SelectedWeapon--;
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             SelectedWeapon = 0;
         }        
-        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        if (Input.GetKeyDown(KeyCode.Alpha2) && NumOfWeapons >= 2)
         {
             SelectedWeapon = 1;
         }        
-        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        if (Input.GetKeyDown(KeyCode.Alpha3) && NumOfWeapons >= 3)
         {
             SelectedWeapon = 2;
         }        
-        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        if (Input.GetKeyDown(KeyCode.Alpha4) && NumOfWeapons >= 4)
         {
             SelectedWeapon = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5) && NumOfWeapons >= 5)
+        {
+            SelectedWeapon = 4;
+        }        
+        if (Input.GetKeyDown(KeyCode.Alpha6) && NumOfWeapons >= 6)
+        {
+            SelectedWeapon = 5;
+        }        
+        if (Input.GetKeyDown(KeyCode.Alpha7) && NumOfWeapons >= 7)
+        {
+            SelectedWeapon = 6;
+        }        
+        if (Input.GetKeyDown(KeyCode.Alpha8) && NumOfWeapons >= 8)
+        {
+            SelectedWeapon = 7;
+        }        
+        if (Input.GetKeyDown(KeyCode.Alpha9) && NumOfWeapons >= 9)
+        {
+            SelectedWeapon = 8;
+        }        
+        if (Input.GetKeyDown(KeyCode.Alpha0) && NumOfWeapons >= 0)
+        {
+            SelectedWeapon = 9;
         }
         if (PreviousSelectedWeapon != SelectedWeapon)
         {
@@ -54,6 +92,44 @@ public class WeaponSwitch : MonoBehaviour
     }
     void SelectWeapon() 
     {
+        useable = false;
+        while (useable == false)
+        {
+            if (gameObject.transform.GetChild(SelectedWeapon).tag == ("Pistol"))
+            {
+                CurrentAmmo = AmmoTracker.PistolAmmo;
+                useable = true;
+            }
+            else if (gameObject.transform.GetChild(SelectedWeapon).tag == ("Rifle"))
+            {
+                CurrentAmmo = AmmoTracker.RifleAmmo;
+            }
+            else if (gameObject.transform.GetChild(SelectedWeapon).tag == ("Sniper"))
+            {
+                CurrentAmmo = AmmoTracker.SniperAmmo;
+            }
+            else if (gameObject.transform.GetChild(SelectedWeapon).tag == ("Heavy"))
+            {
+                CurrentAmmo = AmmoTracker.HeavyAmmo;
+            }
+
+            if (CurrentAmmo == 0)
+            {
+                if (SelectedWeapon == 0)
+                {
+                    SelectedWeapon = NumOfWeapons - 1;
+                }
+                else
+                {
+                    SelectedWeapon--;
+                }
+            }
+            else 
+            {
+                useable = true;
+            }
+        }
+
         int i = 0;
         foreach (Transform weapon in transform)
         {
@@ -65,6 +141,16 @@ public class WeaponSwitch : MonoBehaviour
             {
                 weapon.gameObject.SetActive(false);
             }
+            i++;
+        }
+    }
+
+    void ReorderWeap() 
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            
             i++;
         }
     }
