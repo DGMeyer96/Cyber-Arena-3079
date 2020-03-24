035f25bcb920b1b6170ea9fe068ff3e912af42b8
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-03-23 17:28:20 -0700

    FIXED IT

--- Assets/Scripts/Bolt/First Try/NetworkCallbacks.cs
+++ Assets/Scripts/Bolt/First Try/NetworkCallbacks.cs
@@ -7,7 +7,7 @@ public class NetworkCallbacks : Bolt.GlobalEventListener
     public override void SceneLoadLocalDone(string map)
     {
         // randomize a position
-        var spawnPosition = new Vector3(Random.Range(-8, 8), 7.1f, Random.Range(-8, 8));
+        var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
 
         // instantiate cube
         BoltNetwork.Instantiate(BoltPrefabs.Player, spawnPosition, Quaternion.identity);
