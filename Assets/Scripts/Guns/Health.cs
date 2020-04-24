e98b9c9cd1a21964607534a6bfe98e5bc01cc09f
Daniel Meyer <dmlegoman96@gmail.com>
2020-04-23 17:20:03 -0700

    Pulled Cameron's changes

---
+++ Assets/Scripts/Guns/Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+
+public class Health : MonoBehaviour
+{
+    public float HealthAmount = 50f;
+    public int randomSpawn;
+    public GameObject temptest;
+    void Start()
+    {
+        temptest = GameObject.Find("SpawnBitch");
+    }
+    public void TakeDamage(float damageTaken)
+    {
+        HealthAmount -= damageTaken;
+        Debug.Log("Health is : " + HealthAmount);
+        if (HealthAmount <= 0)
+        {
+            randomSpawn = Random.Range(0, 7);
+            Death();
+        }
+    }
+    //TODO I need to have it respawn rather than delete self
+    void Death()
+    {
+        // gameObject.SetActive(false);
+        //Destroy(gameObject);
+        Debug.Log("I SWEAR TO GOD THIS BETTER COME BACK RIGHT" + temptest.GetComponent<MP_Level_1_Spawn_Data>().spawnPositions[randomSpawn]);
+
+        Vector3 spawnposition = temptest.GetComponent<MP_Level_1_Spawn_Data>().spawnPositions[randomSpawn];
+        Debug.Log("I SWEAR TO GOD THIS BETTER COME BACK RIGHT" + spawnposition);
+
+        Quaternion spawnrotation = temptest.GetComponent<MP_Level_1_Spawn_Data>().spawnRotations[randomSpawn];
+        //BoltNetwork.Instantiate(BoltPrefabs.Player, spawnposition, spawnrotation);
+        gameObject.transform.position = spawnposition;
+        Debug.Log("Hello?");
+        Debug.Log("Should be correct" + gameObject.transform.position);
+        gameObject.transform.rotation = spawnrotation;
+        HealthAmount = 50f;
+
+    }
+}
--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -5,89 +5,141 @@ public class Gun : MonoBehaviour
 {
     public float Damage = 10f;
     public float Range = 100f;
-    public float FireRate = 15f;
-    public float ImpactForce = 30f;
-
+    public float FireRate = 15f; //lower the number the higher the rate NEVER set to 1
     public bool IsAutomatic = false;
     public GameObject sparkattack;
-    public Camera fpsCamera;
-    public VisualEffect Bulletfire;
-    public static readonly string Target = "Target";
-    public static readonly string Velocity = "velocity";
-    public static readonly string position = "position";
-    public static readonly string SpawnLocation = "SpawnLocation";
+    public VisualEffect hitaffect;                  //The actual visual affect itself. We are grabbing it so we can edit the direction the sparks move depending on what wall they hit
+    public float spreadfactor = 0.001f;             //Example we dont want the player to fire at a wall and for it to go the opposit direction of the spark
+    public static readonly string sparkdirection = "VelocityDirection";
+    public VisualEffect Muzzleflash;
+
+    private Vector3 reflection;
+    private readonly float firecooldown = 1f;         //Temp cooldown of firing weapon from shot to shot works with NextTimeToFire
+    private float NextTimeToFire = 0f;      //Temp cooldown of firing weapon works with fire rate and then checks its size vs firecooldown to run
 
 
 
 
-    private float NextTimeToFire = 0f;
+    /*
+    TODO:
+    Fix whatever the hell this fire rate is  ->DONE<-
+    Broken prefab for testing missing stuff and says I cant add anything ->DONE<-
+    direction of spawning sparks ->It doesn't work as intended but I don't fucking care. I am done
+    intensity of said mentioned sparks
+    Rebuild the muzzle flash (again :(  )  muzzle flash is pulled because reflections really fuck with it rn
+    Get respawn working
+    */
+    /*
+    Fire rate -> the lower the number the higher the rate  
+      
+    */
 
-    // Update is called once per frame
     void Update()
     {
         if(IsAutomatic)
         {
-            if (Input.GetButton("Fire1") && Time.time >= NextTimeToFire)
+            NextTimeToFire += Time.deltaTime + 1f / FireRate;
+            if (Input.GetButton("Fire1"))
             {
-                NextTimeToFire = Time.time + 1f / FireRate;
-                Shoot();
-                Debug.Log("Firing Gun - Full-Auto");
-            }
-            else if (!(Input.GetButton("Fire1")))
-            {
+             //   Muzzleflash.SendEvent("OnFire");
+                Debug.Log(NextTimeToFire);
 
+                if (NextTimeToFire > firecooldown)
+                {
+                    NextTimeToFire = 0;
+                    Debug.Log("fires");
+                    Shoot();
+                }
+            }
+            else
+            {
+          //      Muzzleflash.SendEvent("OnStopFire");
             }
         }
-        else
-        {
+        else  // Single fire this is ok but what if player fires as fast as they can pull trigger? More damage than full auto *MUST fix*
+        {       //For now I will do it similar to the full auto check, but I question if there is a better alternative, maybe sit down and think of alternate ideas to run better
+            NextTimeToFire += Time.deltaTime + 1f / FireRate;
             if (Input.GetButtonDown("Fire1"))
             {
-                Shoot();
-                Debug.Log("Firing Gun - Semi-Auto");
-            }
-            else if (!(Input.GetButtonDown("Fire1")))
-            {
-            }
+                Debug.Log(NextTimeToFire);
 
-
+                if (NextTimeToFire > firecooldown)
+                {
+          //          Muzzleflash.SendEvent("OnFire");
+                    NextTimeToFire = 0;
+                    Debug.Log("fires");
+                    Shoot();
+        //            Muzzleflash.SendEvent("OnStopFire");
+                }
+            }
         }
-
-
     }
 
     void Shoot()
     {
-
-        RaycastHit hit;
-
-       Vector3 newvelocity;
-
-
-        Debug.Log("Gets here");
-        if (Physics.Raycast(transform.position, fpsCamera.transform.forward, out hit,Mathf.Infinity))
+        Vector3 direction = transform.forward;
+        direction.x += Random.Range(-spreadfactor, spreadfactor);
+        direction.y += Random.Range(-spreadfactor, spreadfactor);
+        direction.z += Random.Range(-spreadfactor, spreadfactor);
+        if (Physics.Raycast(transform.position, direction, out RaycastHit hit,Range))
         {
-            Debug.Log(hit.transform.name);
-            //temptransform.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+
+
+
+
+
+
+
+
+
+            Debug.DrawLine(transform.position, hit.point, Color.green, 5.0f);
+
+
+
+            reflection = Vector3.Reflect(transform.position, hit.normal);     //Wall directions are not the same so I can't use vector.reflect, but it doesn't matter anyway because the particles are not following the vector.reflect
             
-            newvelocity = transform.InverseTransformPoint(hit.point);
-            Debug.Log("This is Target" + hit.point);
-            Debug.DrawLine(transform.position, hit.point, Color.green);
-            Enemy enemy = hit.transform.GetComponent<Enemy>();//This will not work wtf change to tags
+            hitaffect.SetVector3(sparkdirection, hit.normal);             //Current issue it won't let me assign my visual effect why not?
+                                                                          //Previous issue resolved, now they are just giving it NaNs google searches have revealed the code is giving someone their grandma will need to research why
+
+
+
+
+            Debug.Log("player position: " + transform.position + " And hit position: " + hit.normal + " And reflection direction" + reflection);
+
+
+            Debug.DrawLine(hit.point, reflection, Color.cyan, 5.0f);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             GameObject temp = Instantiate(sparkattack, hit.point, Quaternion.identity);
             Destroy(temp, 1.0f);
-
-
-/*          So make health scripts and actually do this
-            if (enemy != null)
+            if(hit.collider.GetComponent<Health>())
             {
-                enemy.TakeDamage(Damage);
+                hit.collider.GetComponent<Health>().TakeDamage(Damage);
             }
-
-            if(hit.rigidbody != null)
-            {
-                hit.rigidbody.AddForce(-hit.normal * ImpactForce);
-            }
-            */
         }
     }
 }
